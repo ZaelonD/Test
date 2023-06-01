a96ea42e679f8f2e6d34d664e8d9dc3c399eefba
Александр Дубровин <xah799@mail.ru>
2023-06-01 22:50:28 +0300

    Initial commit

--- src/main/java/task4/Solution.java
+++ src/main/java/task4/Solution.java
@@ -1,18 +1,11 @@
 package task4;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
         Read read = new Read(args);
         read.readFile();
         ConvertArray convertArray = new ConvertArray(read.getStrings());
         convertArray.convert();
-        System.out.println(Calculate.minStepsCount(convertArray.getArray()));
+        System.out.print(Calculate.minStepsCount(convertArray.getArray()));
     }
 }
