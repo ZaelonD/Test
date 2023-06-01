4d9aca34bbb6178578df23548dce105663793ed3
Александр Дубровин <xah799@mail.ru>
2023-06-01 22:53:10 +0300

    Initial commit

--- src/main/java/task1/Solution.java
+++ src/main/java/task1/Solution.java
@@ -2,7 +2,6 @@ package task1;
 
 public class Solution {
     public static void main(String[] args) {
-
         CircularArray circularArray = new CircularArray(args[0], args[1]);
         circularArray.print(circularArray.CalculatePath());
     }
